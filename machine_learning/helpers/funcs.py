1f5d304f310cbb21e243bf7a3e27fe818a5e4095
Minh VU <ngoc.vu@polytechnique.org>
2017-05-16 21:08:55 +0100

    add note on rbm

--- machine_learning/helpers/funcs.py
+++ machine_learning/helpers/funcs.py
@@ -1,4 +1,6 @@
 import numpy as np
 
 def sigmoid(x):
+    # clip x between -30 and 30
+    x = np.maximum(-30, np.minimum(30, x))
     return 1.0/(1.0 + np.exp(-x))
--- machine_learning/helpers/rbm.py
+++ machine_learning/helpers/rbm.py
@@ -8,7 +8,9 @@ def sample_bin_from_prob(probs):
     return output
 
 def goodness_grad(visible_state, hidden_state):
+    # get number of samples
     N = visible_state.shape[0]
+
     dW_vh = np.dot(visible_state.T, hidden_state) / N
     db_v  = np.mean(visible_state, axis=0)
     db_h  = np.mean(hidden_state, axis=0)
@@ -45,30 +47,48 @@ class RBMachine(object):
         v_probs = self.hidden_to_visible_probs(h)
         return h, v_probs
 
-    def cd1(self, visible_state):
+    def gibbs_hvh(selfs, h_probs):
+        h, v_probs = selfs.gibbs_h2v(h_probs)
+        v, h_probs = selfs.gibbs_v2h(v_probs)
+        return h, v, h_probs
+
+    def cdk(self, visible_state, cd_steps = 1, persistent = None):
         # since visibale_state can be image intensity which has value in [0,1]
         # we sample a binary value from it
         v0, h0_probs = self.gibbs_v2h(visible_state)
-        h0, v1_probs = self.gibbs_h2v(h0_probs)
-        v1, h1_probs = self.gibbs_v2h(v1_probs)
-        h1 = sample_bin_from_prob(h1_probs)
+        h0 = None
+        v  = None
+        if persistent is None:
+            h_probs = h0_probs
+        else:
+            h_probs = persistent
+
+        for i in range(cd_steps):
+            h, v, h_probs = self.gibbs_hvh(h_probs)
+            if (i==0):
+                h0 = h
+        h = sample_bin_from_prob(h_probs)
 
         dW_vh_0, db_v_0, db_h_0 = goodness_grad(v0, h0)
-        dW_vh_1, db_v_1, db_h_1 = goodness_grad(v1, h1)
+        dW_vh_k, db_v_k, db_h_k = goodness_grad(v, h)
 
-        # we compute update
-        dW_vh = dW_vh_0 - dW_vh_1
-        db_v = db_v_0 - db_v_1
-        db_h = db_h_0 - db_h_1
+        # we compute CD1 update
+        dW_vh = dW_vh_0 - dW_vh_k
+        db_v  = db_v_0 - db_v_k
+        db_h  = db_h_0 - db_h_k
 
-        return dW_vh, db_v, db_h
+        persistent = h
 
-    def step(self, batch_data, learning_rate):
-        dW_vh, db_v, db_h = self.cd1(batch_data)
+        return dW_vh, db_v, db_h, persistent
+
+    def step(self, batch_data, learning_rate, cd_steps = 1, persistent = None):
+        dW_vh, db_v, db_h, persistent = self.cdk(batch_data, cd_steps = cd_steps, persistent = persistent)
 
         self._v_biases += learning_rate * db_v
         self._h_biases += learning_rate * db_h
         self._vh_weights += learning_rate * dW_vh
+
+        return persistent
 
     def get_batches(self, train_data, batch_size):
         N = train_data.shape[0]
@@ -78,11 +98,14 @@ class RBMachine(object):
         for i in range(nb_batch):
             yield train_data[idx[i*batch_size: (i+1)*batch_size]]
 
-    def train(self, train_data, validation_data, epochs, batch_size, learning_rate = 1e-3, prive_every=50):
+    def train(self, train_data, validation_data, epochs, batch_size, learning_rate = 1e-3, prive_every=50, cd_steps = 1, use_pcd = False):
         steps = 0
+        persisten = None
         for i in range(epochs):
             for batch_data in self.get_batches(train_data, batch_size):
-                self.step(batch_data, learning_rate)
+                persisten = self.step(batch_data, learning_rate, cd_steps = cd_steps, persistent = persisten)
+                if not use_pcd:
+                    persisten = None
                 steps += 1
                 if steps % prive_every == 0:
                     loss = self.loss(validation_data)
@@ -96,3 +119,13 @@ class RBMachine(object):
 
         return np.mean(np.square(validation_data - v1_probs))
 
+    def sample(self, v0_prob, num_gibbs_step = 500):
+        v_prob = v0_prob
+        h_prob = None
+
+        v = None
+        h = None
+        for i in range(num_gibbs_step):
+            v, h_prob = self.gibbs_v2h(v_prob)
+            h, v_prob = self.gibbs_h2v(h_prob)
+        return v, v_prob
