2b76d0a95e122e9c95dae0b8f427906fa61ea8f7
Minh VU <ngoc.vu@polytechnique.org>
2017-05-20 15:55:17 +0100

    add deploy script and improve the nb2hugo script to handle copied images files

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+if [ "$#" -ne 1 ]; then
+    echo "You musht specify output hugo dir"
+    exit 1
+fi
+
+INDIR=$(pwd)
+OUTDIR=$1
+
+deploy_one_nb() {
+    cmd="python nb2hugo.py --input_nb $INDIR/$1 --hugo_dir $OUTDIR --outfile content/$2"
+    echo "====================================="
+    printf "Running cmd=%s\n" "$cmd"
+    echo "-------------------------------------"
+    eval $cmd
+}
+
+deploy_one_nb "machine_learning/index.ipynb" "blog/machine_learning.md"
+deploy_one_nb "machine_learning/supervised/linear_regression_part01.ipynb" "blog/machine_learning/supervised/linear_regression_part01.md"
+deploy_one_nb "machine_learning/supervised/linear_regression_part02.ipynb" "blog/machine_learning/supervised/linear_regression_part02.md"
+deploy_one_nb "machine_learning/unsupervised/rbm.ipynb" "blog/machine_learning/unsupervised/rbm.md"
--- nb2hugo.py
+++ nb2hugo.py
@@ -6,7 +6,8 @@ from bs4 import BeautifulSoup
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert notebook to hugo mardown')
     parser.add_argument('--input_nb',   action='store', help='path to notebook')
-    parser.add_argument('--output',     action='store', help='path to output')
+    parser.add_argument('--hugo_dir',   action='store', help='path to hugo directory')
+    parser.add_argument('--outfile',    action='store', help='relative path to hugo directory')
 
     args = parser.parse_args()
 
@@ -17,16 +18,22 @@ if __name__ == '__main__':
     if not os.path.isfile(meta_file):
         raise Exception('Can NOT open input notebook\'s meta-file <{}>'.format(meta_file))
 
-    dir = os.path.dirname(args.output)
-    if not os.path.isdir(dir):
-        os.makedirs(dir)
+    indir = os.path.dirname(args.input_nb)
+    output = os.path.join(args.hugo_dir, args.outfile)
+    outdir = os.path.dirname(output)
+    if not os.path.isdir(outdir):
+        os.makedirs(outdir)
 
     cmd = 'jupyter nbconvert --to html --template basic {}'.format(args.input_nb)
 
     subprocess.check_output(cmd.split())
-    output = '{}.html'.format(args.input_nb[:-len('.ipynb')])
+    out_html = '{}.html'.format(args.input_nb[:-len('.ipynb')])
+    img_files = []
+    out_imgs = []
 
-    with open(output, 'r') as f:
+    fname, fext = os.path.splitext(os.path.basename(output))
+
+    with open(out_html, 'r') as f:
         body = f.read()
 
         # # replace link
@@ -37,16 +44,32 @@ if __name__ == '__main__':
                 a['href'] = a['href'][:-len('.ipynb')] + '/'
 
         for a in soup.findAll('img'):
-            if a['src'].startswith('../assets'):
-                a['src'] = a['href'][:-len('.ipynb')] + '/'
+            img_files.append(os.path.normpath(os.path.join(indir, a['src'])))
+            out_imgs.append(os.path.normpath(os.path.join(outdir, fname, a['src'])))
 
         body = soup.prettify()
+
+    # done with html file, remove it now
+    os.remove(out_html)
+
+    if len(img_files) > 0:
+        print ('\nStart copying images to hugo dir...\n-----------------------------------')
+        for a, b in zip(img_files, out_imgs):
+            if not os.path.isfile(a):
+                continue
+            bdir = os.path.dirname(b)
+            if not os.path.isdir(bdir):
+                os.makedirs(bdir)
+            cmd = 'cp -r {} {}'.format(a, b)
+            subprocess.check_output(cmd.split())
+            print ('copied {} => {} done'.format(a, b))
+        print ("\n")
 
     with open(meta_file, 'r') as f:
         meta = f.read()
 
-    with open(args.output, 'w') as f:
+    with open(output, 'w') as f:
         f.write('{}\n'.format(meta))
         f.write('{}'.format(body))
 
-    print ('Convert notebook {} using meta {} ==> {}\n'.format(args.input_nb, meta_file, args.output))
+    print ('Convert notebook {} using meta {} ==> {}\n=================================\n'.format(args.input_nb, meta_file, output))
